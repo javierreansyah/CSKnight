332f737479bdce9528a7d6874164f8dc7a78a793
Javier <javierreansyah@gmail.com>
2024-05-29 17:00:51 +0700

    jadi matinya jika salah input jawaban

--- scripts/GameManager.gd
+++ scripts/GameManager.gd
@@ -5,6 +5,7 @@ extends Node
 @onready var win_label = $"../CanvasLayer/WinLabel"
 @onready var score_label = $"../CanvasLayer/ScoreLabel"
 @onready var scroll = $"../Player/Scroll"
+@onready var timer = $Timer
 
 func _ready():
 	player.connect("player_died", Callable(self, "_on_player_died"))
@@ -16,6 +17,10 @@ func _ready():
 func _on_player_died():
 	death_label.text = "You Died!"
 	death_label.visible = true
+	timer.start()
+	
+func _on_timer_timeout():
+	get_tree().reload_current_scene()
 
 #Scoring
 var score = 0
@@ -31,4 +36,7 @@ func add_point():
 func on_player_win():
 	win_label.text = "You Win!"
 	win_label.visible = true
+
+
+
 
--- scripts/kill_zone.gd
+++ scripts/kill_zone.gd
@@ -7,7 +7,3 @@ func _on_body_entered(body):
 	if body.is_in_group("player"): 
 		body.die()
 		emit_signal("player_entered_killzone")
-		timer.start()
-
-func _on_timer_timeout():
-	get_tree().reload_current_scene()
